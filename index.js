86adfb2a4c2daeb864a1ce483e1fb5f44984fc83
Evan Kirkiles <evkirkiles@gmail.com>
2018-11-12 22:40:21 -0500

    Rudimentary scrape working.

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 // Import the scrape module
 const coasterSearch = require('./src/scrape_1')
+// Line reader to read in the names of the roller coasters
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('./data/rollercoasterdata.csv')
+});
 
-coasterSearch(1)
+// Constants
+const NUMPAGES = 38
+
+// Read in the data into an array and then get only the first column to compare
+let names = []
+let index = 0
+lineReader.on('line', (line) => {
+	if (index > 0 && index < 300) {
+		names.push(line.substr(0, line.indexOf(',')))
+	}
+	index++
+})
+
+// Once file is finished being read, use the names to find rollercoaster matches
+lineReader.on('close', () => {
+	// Iterate through all pages to check for coasters
+	let numFound = 0
+	for (let i = 1; i <= NUMPAGES; i++) {
+		coasterSearch(i, names, (fulldata) => {
+			numFound += Object.keys(fulldata).length
+			console.log('NUMBER FOUND SO FAR: ' + numFound)
+		})
+	}
+})
+
+
--- src/scrape_1.js
+++ src/scrape_1.js
@@ -2,16 +2,17 @@
 const request = require('request')
 // Require cheerio for page navigation
 const cheerio = require('cheerio')
+// Csv parser for reading in the csv of names
+
 
 // Constants
-const NUMPAGES = 38
 const BASEURL = 'https://captaincoaster.com/en/ranking/coasters?filters%5Bcontinent%5D&filters%5BmaterialType%5D&page='
 
-// First, retrieve the list of names from the excel spreadsheet.
+// First, retrieve the list of names from the excel spreadsheet using csv-parse	
 
 // Build a list of all the rollercoasters on a page, checking against the list of names for each one
 // and discarding those not found in the list of rollercoasters.
-module.exports = function getCoasters(page) {
+module.exports = function checkCoasters(page, names, callback) {
 	request.get({
 		url: BASEURL + page,
 		headers: {
@@ -25,11 +26,12 @@ module.exports = function getCoasters(page) {
 
 		// Now use Cheerio to parse through the body and get each coaster
 		const $ = cheerio.load(body)
+		let fulldata = {}
 		$('ul[class="media-list content-group"]').find('li').each(function(i, element) {
-			var data = {};
+			let data = {};
 			// Manipulate the string to build an object containing all data
 			$(element).find('h2 > a').each(function(i, subelement) {
-				var parts = $(subelement).text().trim().split("-")
+				let parts = $(subelement).text().trim().split("-")
 				data['rank'] = parts[0].trim()
 				data['name'] = parts[1].trim()
 			})
@@ -39,8 +41,15 @@ module.exports = function getCoasters(page) {
 
 			// Print out the data for debugging purposes
 			if (data['rank'] && data['name'] && data['score']) {
-				console.log(data['rank'] + ': ' + data['name'] + ', ' + data['score'])
+				// Find coasters whose names are the exact same (need to expand upon this to become more encompassing (maybe use find?))
+				if (names.indexOf(data['name']) != -1) {
+					fulldata[data['name']] = data
+					console.log(data['rank'] + ': ' + data['name'] + ', ' + data['score'])
+				}
 			}
 		})
+
+		// Return the full data once it is filled
+		callback(fulldata)
 	})
 }
